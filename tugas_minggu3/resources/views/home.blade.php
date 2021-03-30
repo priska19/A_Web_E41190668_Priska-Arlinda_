980f3a7192f575063de00af74c3ee74bc7254d67
priska19 <priska360@gmail.com>
2021-03-30 11:50:39 +0700

    Tugas Minggu 4

--- tugas_minggu3/resources/views/home.blade.php
+++ tugas_minggu3/resources/views/home.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="jumboteon jumbotron-fluid">
+
+<div class="jumboteon jumbotron-fluid">
         <div class="conteiner">
             <h1 class="display-4">Home Page</h1>
             <p class="lead">This is the Home Page</p>
--- tugas_minggu3/routes/web.php
+++ tugas_minggu3/routes/web.php
@@ -18,8 +18,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
 //Route::get('/', 'ManagementUserController');
-Route::resource('/user', ManagementUserController::class);
+
+//Route::resource('/user', ManagementUserController::class);
 // Route::get("/home", function(){
 //     return view("home");
 // });
 
+Route::group(['namespace' => 'Frontend'], function(){
+    Route::resource('home', 'HomeController');
+});
